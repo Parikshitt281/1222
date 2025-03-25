23f43c43ab9f57f8333979de0962c1c8ef8c509b
root <root@ip-172-31-29-164.eu-west-2.compute.internal>
2025-03-25 13:33:29 +0000

    first

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+
+    region = "eu-west-2"
+
+
+}
+
+
+
+resource "aws_instance" "dev" {
+
+      ami = "ami-0e56583ebfdfc098f"
+     instance_type = "t2.micro"
+
+    tags = {
+    Name = "devinstance"
+  }
+}
